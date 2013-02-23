a9036f0ae0ad9f15570feedd542dff74e228361c
Krys <aknadresneg@gmail.com>
2013-02-23 15:41:29 -0500

    Add isolated shader scripts
    thanks to scgruber

---
+++ shader.js
@@ -0,0 +1,57 @@
+// Global shader list
+
+var shaderScripts = {};
+
+shaderScripts['frag'] = {
+  type: 'x-shader/x-fragment',
+  source: [
+    'precision mediump float;',
+    '',
+    'varying vec4 vColor;',
+    '',
+    'void main(void) {',
+    '  gl_FragColor = vColor;',
+    '}'
+  ].join('\n')
+};
+
+shaderScripts['vert'] = {
+  type: 'x-shader/x-vertex',
+  source: [
+    'attribute vec3 aVertexPosition;',
+    'attribute vec4 aVertexColor;',
+    '',
+    'uniform mat4 uMVMatrix;',
+    'uniform mat4 uPMatrix;',
+    '',
+    'varying vec4 vColor;',
+    '',
+    'void main(void) {',
+    '  gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);',
+    '  vColor = aVertexColor;',
+    '}'
+  ].join('\n')
+};
+
+function getShader(name) {
+  if (!shaderScripts[name]) return null;
+
+  var shader;
+  if(shaderScripts[name].type == 'x-shader/x-fragment') {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScripts[name].type == 'x-shader/x-vertex') {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;
+  }
+
+  gl.shaderSource(shader, shaderScripts[name].source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+}
