8c78dbfa922fe83c838c34541b00e3c5f14580d3
Malcolm Inglis <me@minglis.id.au>
2013-04-30 11:16:14 +1000

    Clean up PlaneView + cheat by changing JSHint

--- app/scripts/PlaneView.js
+++ app/scripts/PlaneView.js
@@ -1,8 +1,10 @@
 define(function( require ) {
     'use strict';
 
+
     var glMatrix  = require( 'glMatrix' ),
         utilities = require( 'utilities' );
+
 
     var PlaneView = function( options ) {
         this.width       = options.width;
@@ -18,13 +20,16 @@ define(function( require ) {
         this.children = [];
     };
 
+
     PlaneView.prototype.renderToTexture = function() {
         this.framebuffer.renderToBuffer( this.view );
     };
 
+
     PlaneView.prototype.intersects = function( coords ) {
         return glMatrix.vec2.distance( coords, this.center ) <= this.width / 2;
     };
+
 
     PlaneView.prototype._transferDistance = function() {
         //TODO: set default distance ( (planeWidth + margin) / 2 )
@@ -35,15 +40,17 @@ define(function( require ) {
         return this.parentLine.distToParent();
     };
 
+
     PlaneView.prototype.createChild = function( options ) {
-        var flCenter    = options.foldingLine.center;
-        var flAngle     = options.foldingLine.orientation;
+        var flCenter = options.foldingLine.center;
+        var flAngle  = options.foldingLine.orientation;
 
         // center of the child plane
         var transDist = this._transferDistance();
+        var radians = utilities.radians( flAngle - 90 );
         var childPlaneCenter = [
-            flCenter[0] + transDist * Math.cos( utilities.radians( flAngle-90 ) ),
-            flCenter[1] + transDist * Math.sin( utilities.radians( flAngle-90 ) ), 
+            flCenter[0] + transDist * Math.cos( radians ),
+            flCenter[1] + transDist * Math.sin( radians ),
             this.center[2]
         ];
 
@@ -59,53 +66,70 @@ define(function( require ) {
             center: childPlaneCenter,
             orientation: childPlaneOrientation,
             view: {
-                eye: childView.eye, 
-                center: childView.center, 
+                eye: childView.eye,
+                center: childView.center,
                 up: childView.up
             },
-            framebuffer: options.framebuffer,
+            framebuffer: options.framebuffer
         });
 
         return newChild;
     };
 
-    PlaneView.prototype._createChildView = function( foldingLine ){
-        var parentView = foldingLine.parentPlane.view;
-        var childView  = {
-            eye: glMatrix.vec3.clone(parentView.eye),
-            center: glMatrix.vec3.clone(parentView.center),
-            up: glMatrix.vec3.clone(parentView.up)
-        };
+
+    PlaneView.prototype._createChildView = function( foldingLine ) {
         var flAngle    = foldingLine.orientation;
         var pPAngle    = foldingLine.parentPlane.orientation;
+        var parentView = foldingLine.parentPlane.view;
+
+        var childView  = {
+            eye: glMatrix.vec3.clone( parentView.eye ),
+            center: glMatrix.vec3.clone( parentView.center ),
+            up: glMatrix.vec3.clone( parentView.up )
+        };
 
         var lineOfSight = glMatrix.vec3.create();
-        glMatrix.vec3.subtract(lineOfSight, parentView.eye, parentView.center);
-        glMatrix.vec3.normalize(lineOfSight, lineOfSight); 
-        
+        glMatrix.vec3.subtract(
+            lineOfSight,
+            parentView.eye,
+            parentView.center
+        );
+        glMatrix.vec3.normalize( lineOfSight, lineOfSight );
+
         var rotationAxis = glMatrix.vec3.create();
         var createRA = glMatrix.quat.create();
-        glMatrix.quat.setAxisAngle( createRA, lineOfSight, 
-            utilities.radians( flAngle + 90 - pPAngle ) );
+        glMatrix.quat.setAxisAngle(
+            createRA,
+            lineOfSight,
+            utilities.radians( flAngle + 90 - pPAngle )
+        );
         glMatrix.vec3.transformQuat( rotationAxis, parentView.up, createRA );
         var rotateView = glMatrix.quat.create();
-        glMatrix.quat.setAxisAngle( rotateView, rotationAxis, utilities.radians( -90 ) );
- 
-        //new eye
-        glMatrix.vec3.transformQuat(childView.eye, childView.eye, rotateView);
-        
-        //new up, first rotation
+        glMatrix.quat.setAxisAngle(
+            rotateView,
+            rotationAxis,
+            utilities.radians( -90 )
+        );
+
+        // new eye
+        glMatrix.vec3.transformQuat( childView.eye, childView.eye, rotateView );
+
+        // new up, first rotation
         var turnView = glMatrix.quat.create();
-        glMatrix.quat.setAxisAngle(turnView, lineOfSight, 
-            utilities.radians( flAngle + 180 - pPAngle ) );
-        glMatrix.vec3.transformQuat(childView.up, childView.up, turnView);
-        glMatrix.vec3.normalize(childView.up, childView.up);
-        
-        glMatrix.vec3.transformQuat(childView.up, childView.up, rotateView);
-        glMatrix.vec3.normalize(childView.up, childView.up);
-        
+        glMatrix.quat.setAxisAngle(
+            turnView,
+            lineOfSight,
+            utilities.radians( flAngle + 180 - pPAngle )
+        );
+        glMatrix.vec3.transformQuat( childView.up, childView.up, turnView );
+        glMatrix.vec3.normalize( childView.up, childView.up );
+
+        glMatrix.vec3.transformQuat( childView.up, childView.up, rotateView );
+        glMatrix.vec3.normalize( childView.up, childView.up );
+
         return childView;
     };
+
 
     return PlaneView;
 });
