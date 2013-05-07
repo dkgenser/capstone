e1c12c00721ea34557c79453301cd1d3f51df351
Krys <aknadresneg@gmail.com>
2013-05-07 01:10:08 -0400

    Edit intersects
    Tests every triangle on the plane. Still buggy though.

--- app/scripts/Plane.js
+++ app/scripts/Plane.js
@@ -21,6 +21,7 @@ define(function( require ) {
         this.wireframe = options.wireframe;
         
         this.selected  = false;
+        this.triangles = this._constructTriangles();
         this._constructBuffers();
         // TODO: bounding sphere
     };
@@ -37,7 +38,7 @@ define(function( require ) {
 
         //Planes buffer
         this.planeBuffer = new BufferSet( { gl: this.gl }, {
-            positions:      this._constructTriangles(),
+            positions:      this._constructTriPositions(),
             colors:         PLANE_SELECT_COLOR,
             textureCoords:  new Float32Array( 2 *( 3 *( this.points.length - 2 ))),
         });
@@ -51,23 +52,41 @@ define(function( require ) {
         return new Float32Array( positions );
     };
 
+    Plane.prototype._constructTriPositions = function() {
+        if ( this.triangles === null ) {
+            this.triangles = this._constructTriangles();
+        }
+
+        var verticies = [];
+
+        for(var i = 0; i < this.triangles.length; i++) {
+            verticies = verticies.concat( this.triangles[i][0] );
+            verticies = verticies.concat( this.triangles[i][1] );
+            verticies = verticies.concat( this.triangles[i][2] );
+        };
+
+        return new Float32Array( verticies );
+    }
+
     Plane.prototype._constructTriangles = function() {
         var first = this.points[0];
         var last  = this.points[1];
         var next;
 
-        var verticies = [];
+        var triangles = [];
 
         for(var i = 2; i < this.points.length; i++) {
             next = this.points[i];
-            verticies = verticies.concat( first );
-            verticies = verticies.concat( last );
-            verticies = verticies.concat( next );
+            var triangle = [];
+            triangle.push( first );
+            triangle.push( last );
+            triangle.push( next );
 
             last = next;
+            triangles.push( triangle );
         };
 
-        return new Float32Array( verticies );
+        return triangles;
     };
 
     Plane.prototype.draw = function() {
@@ -87,7 +106,7 @@ define(function( require ) {
             this.planeBuffer.assignVertexAttributes( this.world.program );
             this.world.setMatrixUniforms();
             this.gl.drawArrays(
-                this.gl.TRIANGLE_STRIP,
+                this.gl.TRIANGLES,
                 0,
                 this.planeBuffer.positions.numItems
             ); 
@@ -102,55 +121,64 @@ define(function( require ) {
         var t   = options.time;
         var min = options.min;
         var max = options.max;
+
+        var intersected = false;
         
-        var a = this.points[0];
-        var b = this.points[1];
-        var c = this.points[2];
+        this.triangles.forEach( function( triangle ) {
+            var a = triangle[0];
+            var b = triangle[1];
+            var c = triangle[2];
 
-        var u = [0, 0, 0];
-        var v = [0, 0, 0];
-        var w = [0, 0, 0];
+            var u = [0, 0, 0];
+            var v = [0, 0, 0];
+            var w = [0, 0, 0];
 
-        glMatrix.vec3.subtract(u, a, b);
-        glMatrix.vec3.subtract(v, a, c);
-        glMatrix.vec3.subtract(w, a, e);
+            glMatrix.vec3.subtract(u, a, b);
+            glMatrix.vec3.subtract(v, a, c);
+            glMatrix.vec3.subtract(w, a, e);
 
-        var eihf = v[1]*d[2] - d[1]*v[2];
-        var gfdi = d[0]*v[2] - v[0]*d[2];
-        var dheg = v[0]*d[1] - d[0]*v[1];
-        var akjb = u[0]*w[1] - w[0]*u[1];
-        var jcal = w[0]*u[2] - u[0]*w[2];
-        var blkc = u[1]*w[2] - w[1]*u[2];
+            var eihf = v[1]*d[2] - d[1]*v[2];
+            var gfdi = d[0]*v[2] - v[0]*d[2];
+            var dheg = v[0]*d[1] - d[0]*v[1];
+            var akjb = u[0]*w[1] - w[0]*u[1];
+            var jcal = w[0]*u[2] - u[0]*w[2];
+            var blkc = u[1]*w[2] - w[1]*u[2];
 
-        var M = u[0]*eihf + u[1]*gfdi + u[2]*dheg;
+            var M = u[0]*eihf + u[1]*gfdi + u[2]*dheg;
+            if ( M === 0 ) {
+                alert( " M = 0 ");
+            }
 
-        //calculated first in hopes of gaining some efficiency
-        var tempt = (v[2]*akjb + v[1]*jcal + v[0]*blkc) / M;
+            //calculated first in hopes of gaining some efficiency
+            var tempt = (v[2]*akjb + v[1]*jcal + v[0]*blkc) / M;
 
-        if( tempt < min || tempt > max )
-        {
-            //alert(tempt);
-            return false;
-        }
+            if( tempt < min || tempt > max )
+            {
+                alert(tempt);
+                return false;
+            }
 
-        if( t !== null ) { t = tempt; }
+            if( t !== null ) { t = tempt; }
 
-        var beta = (w[0]*eihf + w[1]*gfdi + w[2]*dheg) / M;
-        //alert ( beta );
+            var beta = (w[0]*eihf + w[1]*gfdi + w[2]*dheg) / M;
+            alert ( beta );
 
-        if( beta<0 || beta>1 ) 
-        {
-            return false;
-        }
+            if( beta<0 || beta>1 ) 
+            {
+                return false;
+            }
 
-        var gamma = (d[2]*akjb + d[1]*jcal + d[0]*blkc) / M;
-        //alert( gamma );
-        if( gamma<0 || gamma > 1 - beta ) 
-        {
-            return false;
-        }
+            var gamma = (d[2]*akjb + d[1]*jcal + d[0]*blkc) / M;
+            alert( gamma );
+            if( gamma<0 || gamma > 1 - beta ) 
+            {
+                return false;
+            }
 
-        return true;
+            intersected = true;
+        });
+
+        return intersected;
     };
 
 
